a4e7a7bf069f905c0521674328c274ce2892a826
dwc05101 <dwc05101@kaist.ac.kr>
2020-10-10 13:46:10 +0900

    :sparkles: Database configuration

--- index.js
+++ index.js
@@ -12,7 +12,16 @@ app.use(cors());
 
 const PORT = 80;
 const BASE_URL = "https://render-api.zepeto.io/v2/graphics/zepeto/booth/";
-const DATABASE = "";
+
+// MongoDB Configuration
+var mongoose = require("mongoose");
+var db = mongoose.connection;
+db.on("error", console.error);
+db.once("open", function () {
+  console.log("Connected to MongoDB server");
+});
+
+mongoose.connect("mongodb://localhost:27017/zepeto-gpd");
 
 var server = app.listen(PORT, function () {
   console.log("Express server running on port " + PORT);
